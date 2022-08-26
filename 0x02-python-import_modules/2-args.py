4c355559790cfcf8ded5ce511a82cb9567d930b8
pascaladechinan <82267569+pascaladechinan@users.noreply.github.com>
2022-08-26 05:59:29 +0100

    Update 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,11 +1,25 @@
 #!/usr/bin/python3
-import sys
 
 if __name__ == "__main__":
-    argc = len(sys.argv) - 1
+    """Print the number of and list of arguments."""
+    import sys
 
-    print("{} argument{}".format(argc, '' if argc == 1 else 's'), end='')
-    print("{}".format('.' if argc == 0 else ':'))
+    count = len(sys.argv) - 1
+    if count == 0:
+        print("0 arguments.")
+    elif count == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(count))
+    for i in range(count):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
 
-    for index in range(1, argc + 1):
-        print("{}: {}".format(index, sys.argv[index]))
+
+
+
+
+
+
+
+
+
