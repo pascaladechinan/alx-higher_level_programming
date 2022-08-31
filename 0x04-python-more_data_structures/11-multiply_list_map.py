6253f95a8949c56ce6ef9a098204a6c037a1670e
pascaladechinan <82267569+pascaladechinan@users.noreply.github.com>
2022-08-31 23:43:52 +0100

    Update 11-multiply_list_map.py

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
-def delete_at(my_list=[], idx=0):
-    if idx < 0 or idx > len(my_list) - 1:
-        return my_list
-    else:
-        del my_list[idx]
-    return my_list
+def multiply_list_map(my_list=[], number=0):
+    return (list(map((lambda i: i * number), my_list)))
