493c00a5085f6902bbac8191308f90d31c9534ff
pascaladechinan <82267569+pascaladechinan@users.noreply.github.com>
2022-08-31 23:31:27 +0100

    Update 8-simple_delete.py

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-def multiple_returns(sentence):
-    my_tuple = ()
-    if len(sentence) == 0:
-        my_tuple = 0, "None"
-    else:
-        my_tuple = len(sentence), sentence[0]
-    return my_tuple
+
+def simple_delete(a_dictionary, key=""):
+    if a_dictionary.get(key) is not None:
+        del a_dictionary[key]
+    return (a_dictionary)
+
+
+
+
